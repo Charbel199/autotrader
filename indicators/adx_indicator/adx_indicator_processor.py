ab6a1fc56c02a06618de5b0065e9c2eac828e5a5
q524948 <charbel.bou-maroun@bmw.de>
2021-09-24 23:43:09 +0200

    Add temp dataframe to improve performance

--- indicators/adx_indicator/adx_indicator_processor.py
+++ indicators/adx_indicator/adx_indicator_processor.py
@@ -15,48 +15,54 @@ class ADX(object):
         self.data_structure = data_structure
 
     def process_new_candlestick(self):
+        # Create temporary dataframe (We only need the last 14 data points)
+        if len(self.df.index) > self.period:
+            temp_df = self.df.tail(self.period).copy()
+        else:
+            temp_df = self.df.copy()
         # Create new row
-        self.df.loc[len(self.df.index)] = {'Time': self.data_structure.get_last_value('Time')}
-
+        temp_df.loc[len(self.df.index)] = {'Time': self.data_structure.get_last_value('Time')}
         if self.data_structure.get_number_of_rows() >= 2:
             # Calculate true range
-            self.df['TrueRange'].iloc[-1] = max(
+            temp_df['TrueRange'].iloc[-1] = max(
                 self.data_structure.get_last_value('High') - self.data_structure.get_last_value('Low'),
                 self.data_structure.get_last_value('High') - self.data_structure.get_before_last_value('Close'),
                 self.data_structure.get_before_last_value('Close') - self.data_structure.get_last_value('Low')
             )
             # Calculate H-pH and pL-L
-            self.df['H-pH'].iloc[-1] = self.data_structure.get_last_value('High') - self.data_structure.get_before_last_value('High')
-            self.df['pL-L'].iloc[-1] = self.data_structure.get_before_last_value('Low') - self.data_structure.get_last_value('Low')
+            temp_df['H-pH'].iloc[-1] = self.data_structure.get_last_value('High') - self.data_structure.get_before_last_value('High')
+            temp_df['pL-L'].iloc[-1] = self.data_structure.get_before_last_value('Low') - self.data_structure.get_last_value('Low')
 
             # Calculate +DX and -DX
-            self.df['+DX'].iloc[-1] = self.df['H-pH'].iloc[-1] if self.df['H-pH'].iloc[-1] > self.df['pL-L'].iloc[-1] and self.df['H-pH'].iloc[-1] > 0 else 0
-            self.df['-DX'].iloc[-1] = self.df['pL-L'].iloc[-1] if self.df['pL-L'].iloc[-1] > self.df['H-pH'].iloc[-1] and self.df['pL-L'].iloc[-1] > 0 else 0
+            temp_df['+DX'].iloc[-1] = temp_df['H-pH'].iloc[-1] if temp_df['H-pH'].iloc[-1] > temp_df['pL-L'].iloc[-1] and temp_df['H-pH'].iloc[-1] > 0 else 0
+            temp_df['-DX'].iloc[-1] = temp_df['pL-L'].iloc[-1] if temp_df['pL-L'].iloc[-1] > temp_df['H-pH'].iloc[-1] and temp_df['pL-L'].iloc[-1] > 0 else 0
 
         # Calculate ATR and smooth +DX and -DX
-        if self.df['TrueRange'].count() == self.period and self.df['ATR'].count() == 0:
-            self.df['ATR'].iloc[-1] = np.mean(self.df['TrueRange'].tail(self.period).tolist())
-            self.df['Smooth+DX'].iloc[-1] = np.mean(self.df['+DX'].tail(self.period).tolist())
-            self.df['Smooth-DX'].iloc[-1] = np.mean(self.df['-DX'].tail(self.period).tolist())
+        if temp_df['TrueRange'].count() == self.period and temp_df['ATR'].count() == 0:
+            temp_df['ATR'].iloc[-1] = np.mean(temp_df['TrueRange'].tail(self.period).tolist())
+            temp_df['Smooth+DX'].iloc[-1] = np.mean(temp_df['+DX'].tail(self.period).tolist())
+            temp_df['Smooth-DX'].iloc[-1] = np.mean(temp_df['-DX'].tail(self.period).tolist())
 
-        elif self.df['ATR'].count() > 0:
-            self.df['ATR'].iloc[-1] = (self.df['ATR'].iloc[-2] * (self.period - 1) + self.df['TrueRange'].iloc[-1]) / self.period
-            self.df['Smooth+DX'].iloc[-1] = (self.df['Smooth+DX'].iloc[-2] * (self.period - 1) + self.df['+DX'].iloc[-1]) / self.period
-            self.df['Smooth-DX'].iloc[-1] = (self.df['Smooth-DX'].iloc[-2] * (self.period - 1) + self.df['-DX'].iloc[-1]) / self.period
+        elif temp_df['ATR'].count() > 0:
+            temp_df['ATR'].iloc[-1] = (temp_df['ATR'].iloc[-2] * (self.period - 1) + temp_df['TrueRange'].iloc[-1]) / self.period
+            temp_df['Smooth+DX'].iloc[-1] = (temp_df['Smooth+DX'].iloc[-2] * (self.period - 1) + temp_df['+DX'].iloc[-1]) / self.period
+            temp_df['Smooth-DX'].iloc[-1] = (temp_df['Smooth-DX'].iloc[-2] * (self.period - 1) + temp_df['-DX'].iloc[-1]) / self.period
 
         # Calculate +DMI and -DMI and DX a
-        if self.df['ATR'].count() > 0:
-            self.df['+DMI'].iloc[-1] = (self.df['Smooth+DX'].iloc[-1] / self.df['ATR'].iloc[-1]) * 100
-            self.df['-DMI'].iloc[-1] = (self.df['Smooth-DX'].iloc[-1] / self.df['ATR'].iloc[-1]) * 100
-            self.df['DX'].iloc[-1] = (abs(self.df['+DMI'].iloc[-1] - self.df['-DMI'].iloc[-1]) / (self.df['+DMI'].iloc[-1] + self.df['-DMI'].iloc[-1]))*100
+        if temp_df['ATR'].count() > 0:
+            temp_df['+DMI'].iloc[-1] = (temp_df['Smooth+DX'].iloc[-1] / temp_df['ATR'].iloc[-1]) * 100
+            temp_df['-DMI'].iloc[-1] = (temp_df['Smooth-DX'].iloc[-1] / temp_df['ATR'].iloc[-1]) * 100
+            temp_df['DX'].iloc[-1] = (abs(temp_df['+DMI'].iloc[-1] - temp_df['-DMI'].iloc[-1]) / (temp_df['+DMI'].iloc[-1] + temp_df['-DMI'].iloc[-1])) * 100
 
         # Calculate ADX
-        if self.df['DX'].count() == self.period and self.df['ADX'].count() == 0:
-            self.df['ADX'].iloc[-1] = np.mean(self.df['DX'].tail(self.period).tolist())
-        elif self.df['DX'].count() > 0:
-            self.df['ADX'].iloc[-1] = (self.df['ADX'].iloc[-2] * (self.period - 1) + self.df['DX'].iloc[-1]) / self.period
+        if temp_df['DX'].count() == self.period and temp_df['ADX'].count() == 0:
+            temp_df['ADX'].iloc[-1] = np.mean(temp_df['DX'].tail(self.period).tolist())
+        elif temp_df['DX'].count() > 0:
+            temp_df['ADX'].iloc[-1] = (temp_df['ADX'].iloc[-2] * (self.period - 1) + temp_df['DX'].iloc[-1]) / self.period
 
-
+        # Update ADX dataframe
+        self.df = self.df.append(temp_df.tail(1))
+        print(self.df.to_string())
 
     def get_last_adx_values(self, n=1):
         # Gets last ADX by default
