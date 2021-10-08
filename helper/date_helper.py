7407c695bd212ef32269cea50fdce6c99b3e3b48
q524948 <charbel.bou-maroun@bmw.de>
2021-10-08 22:33:46 +0200

    Add more functionalities for date_helper

--- helper/date_helper.py
+++ helper/date_helper.py
@@ -1,6 +1,8 @@
 import time
 from datetime import datetime
 import datetime as dt
+import random
+
 
 def from_binance_date_to_timestamp(date):
     try:
@@ -12,10 +14,38 @@ def from_binance_date_to_timestamp(date):
 
 def from_timestamp_to_binance_date(timestamp):
     try:
-        date = datetime.fromtimestamp(timestamp).strftime("%d %b, %Y")
+        date = datetime.fromtimestamp(timestamp).strftime("%d %b %Y %H:%M ")
         return date
     except:
         return
+
+
+def get_previous_timestamp(timestamp, hours=0, minutes=0):
+    try:
+        if hours != 0:
+            timestamp -= 3600 * hours
+        if minutes != 0:
+            timestamp -= 60 * minutes
+        return timestamp
+    except:
+        return
+
+
+def get_next_timestamp(timestamp, hours=0, minutes=0):
+    try:
+        if hours != 0:
+            timestamp += 3600 * hours
+        if minutes != 0:
+            timestamp += 60 * minutes
+        return timestamp
+    except:
+        return
+
+
+def get_random_timestamp(lower_bound_timestamp, upper_bound_timestamp=None):
+    if upper_bound_timestamp is None:
+        upper_bound_timestamp = get_current_timestamp()
+    return random.randint(lower_bound_timestamp, upper_bound_timestamp)
 
 
 def from_timestamp_to_date(timestamp):
@@ -28,4 +58,3 @@ def from_timestamp_to_date(timestamp):
 
 def get_current_timestamp():
     return int(datetime.now(dt.timezone.utc).timestamp())
-
