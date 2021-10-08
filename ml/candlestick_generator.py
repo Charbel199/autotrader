8d79b81a7b58131dbf62dfad81c024ec335ba18c
q524948 <charbel.bou-maroun@bmw.de>
2021-10-08 22:33:09 +0200

    Add candlestick generator functionalities

--- ml/candlestick_generator.py
+++ ml/candlestick_generator.py
@@ -1,18 +1,32 @@
 from data.previous_data.data_fetcher import DataFetcher
 import collections
+import random
+from helper import date_helper
 
 
 class CandlestickGenerator(object):
     data_fetcher: DataFetcher
 
-    def __init__(self, data_fetcher, symbols):
+    def __init__(self, data_fetcher, symbols, timeframe):
         self.buffer = collections.deque()
         self.symbols = symbols
         self.data_fetcher = data_fetcher
+        self.timeframe = timeframe
         pass
 
-    def fetch_new_candlesticks(self):
-        pass
-
+    def fetch_new_candlesticks(self, start_date, duration):
+        print(date_helper.from_timestamp_to_binance_date(start_date))
+        print( date_helper.from_timestamp_to_binance_date(
+                date_helper.get_next_timestamp(start_date, hours=duration)))
+        candlesticks = self.data_fetcher.get_candlesticks(
+            random.choice(self.symbols),
+            self.timeframe,
+            date_helper.from_timestamp_to_binance_date(start_date),
+            date_helper.from_timestamp_to_binance_date(
+                date_helper.get_next_timestamp(start_date, hours=duration))
+        )
+        self.buffer.append(candlesticks)
+        print("Deque ",self.buffer)
+        print("len of dq ",len(self.buffer) )
     def get_new_candlesticks(self):
-        pass
+        return self.buffer.pop()
