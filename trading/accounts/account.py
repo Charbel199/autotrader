203c528ccc393cd13be69fb2a55a3a39f03d4b41
q524948 <charbel.bou-maroun@bmw.de>
2021-10-12 16:25:26 +0200

    Remove last buy when getting profit

--- trading/accounts/account.py
+++ trading/accounts/account.py
@@ -22,12 +22,17 @@ class Account(ABC):
         pass
 
     def get_profit(self):
+        if self.df["Action"].iloc[-1] == "Buy":
+            self.df = self.df[:-1]
         sells = self.df[self.df["Action"] == "Sell"]
         buys = self.df[self.df["Action"] == "Buy"]
         total_buy_price = (buys["Price"] * buys["Amount"]).sum()
         total_sell_price = (sells["Price"] * sells["Amount"]).sum()
         profit = total_sell_price - total_buy_price
-        percentage = ((total_sell_price - total_buy_price) / total_buy_price) * 100
+        if total_buy_price != 0:
+            percentage = ((total_sell_price - total_buy_price) / total_buy_price) * 100
+        else:
+            percentage = 0
         print("Buy ", total_buy_price)
         print("Sell ", total_sell_price)
         print("Percentage gain: ", percentage)
