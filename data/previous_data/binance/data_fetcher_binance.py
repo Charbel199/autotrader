4dee14c2c3c06303a6b1d102b1e175982f222ad4
q524948 <charbel.bou-maroun@bmw.de>
2021-10-22 19:00:00 +0200

    Add save and load functionalities for candlesticks

--- data/previous_data/binance/data_fetcher_binance.py
+++ data/previous_data/binance/data_fetcher_binance.py
@@ -3,6 +3,9 @@ from binance.client import Client
 import os
 from data.data_logger import logger
 from helper import date_helper
+import pickle
+import os
+
 log = logger.get_logger(__name__)
 
 
@@ -21,25 +24,29 @@ class DataFetcherBinance(DataFetcher):
     def get_candlesticks(self, symbol, timeframe, start_date, end_date=None):
         log.info(f'Fetching candlesticks for {symbol}, timeframe of {timeframe} and a start date of {start_date} and an end date of {end_date}')
         try:
-            client = Client(os.getenv('API_KEY'), os.getenv('API_SECRET'))
+            candlesticks = load_candlesticks(symbol, timeframe, start_date, end_date)
+            if len(candlesticks) == 0:
+                client = Client(os.getenv('API_KEY'), os.getenv('API_SECRET'))
 
-            # Format timeframe
-            if timeframe in self.binance_timeframes:
-                binance_timeframe = self.binance_timeframes[timeframe]
-            else:
-                binance_timeframe = None
-
-            # Get candlesticks
-            if binance_timeframe:
-                if end_date:
-                    candlesticks = client.get_historical_klines(symbol, binance_timeframe, start_date, end_date)
+                # Format timeframe
+                if timeframe in self.binance_timeframes:
+                    binance_timeframe = self.binance_timeframes[timeframe]
                 else:
-                    candlesticks = client.get_historical_klines(symbol, binance_timeframe, start_date)
-            else:
-                return []
-            candlesticks = list(map(format_tick, candlesticks))
-            # Close connection
-            client.close_connection()
+                    binance_timeframe = None
+
+                # Get candlesticks
+                if binance_timeframe:
+                    if end_date:
+                        candlesticks = client.get_historical_klines(symbol, binance_timeframe, start_date, end_date)
+                    else:
+                        candlesticks = client.get_historical_klines(symbol, binance_timeframe, start_date)
+                else:
+                    return []
+                candlesticks = list(map(format_tick, candlesticks))
+                save_candlesticks(symbol, timeframe, start_date, end_date, candlesticks)
+                # Close connection
+                client.close_connection()
+
             log.info('Finished fetching candlesticks')
             return candlesticks
         except Exception:
@@ -64,3 +71,31 @@ def format_tick(tick):
         "CloseTime": tick[6]
     }
     return candlestick
+
+
+def load_candlesticks(symbol, timeframe, start_date, end_date):
+    try:
+        if end_date is not None:
+            directory = "saved_data"
+            existing_files = [f for f in os.listdir(directory) if f.endswith('.pkl')]
+            file_name = f"{symbol}_{timeframe}_{start_date}_{end_date}.pkl"
+            if file_name in existing_files:
+                with open(f"{directory}/{file_name}", 'rb') as f:
+                    return pickle.load(f)
+            else:
+                return []
+    except Exception:
+        return []
+
+
+def save_candlesticks(symbol, timeframe, start_date, end_date, list):
+    try:
+        if end_date is not None:
+            directory = "saved_data"
+            existing_files = [f for f in os.listdir(directory) if f.endswith('.pkl')]
+            file_name = f"{symbol}_{timeframe}_{start_date}_{end_date}.pkl"
+            if file_name not in existing_files:
+                with open(f"{directory}/{file_name}", 'wb') as f:
+                    pickle.dump(list, f)
+    except Exception:
+        return
