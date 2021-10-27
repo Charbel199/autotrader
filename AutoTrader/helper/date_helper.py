bf3d75b613d6956386852ff548913226851c0db5
q524948 <charbel.bou-maroun@bmw.de>
2021-10-27 15:43:49 +0200

    Add from seconds to time

--- AutoTrader/helper/date_helper.py
+++ AutoTrader/helper/date_helper.py
@@ -9,7 +9,7 @@ def from_binance_date_to_timestamp(date: str) -> int:
         timestamp = time.mktime(datetime.strptime(date, "%d %b, %Y").timetuple())
         return int(timestamp)
     except:
-        raise Exception(f"Couldn't convert {date} to timestamp.")
+        raise ValueError(f"Couldn't convert {date} to timestamp.")
 
 
 def from_timestamp_to_binance_date(timestamp: int) -> str:
@@ -17,7 +17,7 @@ def from_timestamp_to_binance_date(timestamp: int) -> str:
         date = datetime.fromtimestamp(timestamp).strftime("%d %b %Y %H:%M ")
         return date
     except:
-        raise Exception(f"Couldn't convert {timestamp} to date.")
+        raise ValueError(f"Couldn't convert {timestamp} to date.")
 
 
 def get_previous_timestamp(timestamp: int, hours: int = 0, minutes: int = 0) -> int:
@@ -28,7 +28,7 @@ def get_previous_timestamp(timestamp: int, hours: int = 0, minutes: int = 0) -> 
             timestamp -= 60 * minutes
         return timestamp
     except:
-        raise Exception(f"Couldn't get previous timestamp from {timestamp} going back {hours} hours and {minutes} minutes.")
+        raise ValueError(f"Couldn't get previous timestamp from {timestamp} going back {hours} hours and {minutes} minutes.")
 
 
 def get_next_timestamp(timestamp: int, hours: int = 0, minutes: int = 0) -> int:
@@ -39,7 +39,7 @@ def get_next_timestamp(timestamp: int, hours: int = 0, minutes: int = 0) -> int:
             timestamp += 60 * minutes
         return timestamp
     except:
-        raise Exception(f"Couldn't get next timestamp from {timestamp} going forward {hours} hours and {minutes} minutes.")
+        raise ValueError(f"Couldn't get next timestamp from {timestamp} going forward {hours} hours and {minutes} minutes.")
 
 
 def get_random_timestamp(lower_bound_timestamp: int, upper_bound_timestamp: int = None) -> int:
@@ -57,8 +57,19 @@ def from_timestamp_to_date(timestamp: int) -> str:
         date = datetime.fromtimestamp(timestamp).strftime("%d %b %Y %H:%M ")
         return date
     except:
-        raise Exception(f"Couldn't convert {timestamp} to date.")
+        raise ValueError(f"Couldn't convert {timestamp} to date.")
 
 
 def get_current_timestamp() -> int:
     return int(datetime.now(dt.timezone.utc).timestamp())
+
+
+def from_seconds_to_time(seconds: int)->str:
+    seconds_in_day = 60 * 60 * 24
+    seconds_in_hour = 60 * 60
+    seconds_in_minute = 60
+
+    days = seconds // seconds_in_day
+    hours = (seconds - (days * seconds_in_day)) // seconds_in_hour
+    minutes = (seconds - (days * seconds_in_day) - (hours * seconds_in_hour)) // seconds_in_minute
+    return f"{days} days, {hours} hours, {minutes} minutes"
