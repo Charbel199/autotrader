12ea382a459e05fb59f596a93a4b38aaa8059924
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-08 04:27:00 +0200

    Change account into portfolio

--- AutoTrader/trading/accounts/account.py
+++ AutoTrader/trading/accounts/account.py
@@ -4,59 +4,66 @@ from AutoTrader.data.models import Position
 import plotly.graph_objects as go
 from AutoTrader.exceptions import AccountNotFound
 from AutoTrader.services.summary import get_trades_summary, print_summary
+from typing import Dict, List
+import copy
 
 log = logger.get_logger(__name__)
 
 
 class Account(ABC):
     columns = ['Time', 'Action', 'Amount', 'Symbol', 'Price']
+    transaction_percentage = NotImplemented
+    transaction_fee = NotImplemented
 
-    def __init__(self, balance: float):
-        self.position = Position()
-        self.initial_balance = balance
-        self.balance = balance
-        self.list = []
+    def __init__(self):
+        self.positions: Dict[str, Position] = {}
+        balance = self._get_balance()
+        self.balance: Dict[str, float] = balance
+        self.initial_balance: Dict[str, float] = copy.deepcopy(balance)
+        self.lists: Dict[str, List[dict]] = {}
+
+    def get_position(self, symbol: str) -> Position:
+        return self.positions[symbol] if symbol in self.positions else Position()
 
     @abstractmethod
-    def get_position(self) -> Position:
-        return self.position
-
-    @abstractmethod
-    def buy(self,
-            time: int,
-            symbol: str,
-            price: float,
-            amount: float = 0) -> None:
+    def transaction(self,
+                    time: int,
+                    symbol: str,
+                    source_symbol: str,
+                    destination_symbol: str,
+                    source_symbol_amount: float,
+                    transaction_type: str,
+                    destination_symbol_amount: float = 0) -> None:
         pass
 
-    @abstractmethod
-    def sell(self,
-             time: int,
-             symbol: str,
-             price: float,
-             amount: float = 0) -> None:
-        pass
-
-    def get_profit(self) -> float:
+    def get_profit(self, symbol: str, primary_symbol: str, secondary_symbol: str) -> float:
         # If in position when calculating profit, revert last buy
-        if self.position.is_valid():
-            self.list = self.list[:-1]
-            self.balance += self.position.Price * self.position.Amount
-            self.position = Position()
-        summary = get_trades_summary(self.list, self.initial_balance)
+        self._undo_last_position(symbol, primary_symbol, secondary_symbol)
+
+        summary = get_trades_summary(self.lists[symbol], self.initial_balance[primary_symbol])
         log.info(print_summary(summary))
-        return summary['PercentageChange']
+        return summary['PercentageChange'] if summary != {} else 0
 
     @abstractmethod
-    def get_plot(self) -> go:
+    def get_plot(self, symbol: str) -> go:
         pass
 
-    def get_all_trades(self) -> list:
-        if self.position.is_valid():
-            self.list = self.list[:-1]
-            self.balance += self.position.Price * self.position.Amount
-            self.position = Position()
-        summary = get_trades_summary(self.list, self.initial_balance)
+    @abstractmethod
+    def _get_balance(self) -> Dict[str, float]:
+        pass
+
+    def _undo_last_position(self, symbol: str, primary_symbol: str, secondary_symbol: str) -> None:
+        self.positions[secondary_symbol] = Position() if secondary_symbol not in self.positions else self.positions[secondary_symbol]
+        if self.positions[secondary_symbol].is_valid():
+            self.lists[symbol] = self.lists[symbol][:-1]
+            self.balance[primary_symbol] += self.positions[secondary_symbol].Price * self.positions[secondary_symbol].Amount
+            self.positions[secondary_symbol] = Position()
+
+    def get_all_trades(self, symbol: str, primary_symbol: str, secondary_symbol: str) -> list:
+        # If in position when getting all trades, revert last buy
+        self._undo_last_position(symbol, primary_symbol, secondary_symbol)
+
+        summary = get_trades_summary(self.lists[symbol], self.initial_balance[primary_symbol])
         return summary['AllTrades']
 
     @staticmethod
@@ -66,8 +73,8 @@ class Account(ABC):
 
 
 # Get strategy
-def get_account(name: str, balance: float) -> Account:
+def get_account(name: str) -> Account:
     for account in Account.__subclasses__():
         if account.condition(name):
-            return account(balance)
+            return account()
     raise AccountNotFound(f"Account type: {name} not found")
--- AutoTrader/trading/accounts/test_account_processor.py
+++ AutoTrader/trading/accounts/test_account_processor.py
@@ -2,67 +2,73 @@ from AutoTrader.trading.accounts.account import Account
 import plotly.graph_objects as go
 from AutoTrader.helper import logger
 from AutoTrader.data.models import Position
+from typing import Dict
 
 log = logger.get_logger(__name__)
 
 
 class TestAccount(Account):
+    transaction_percentage = 0.1 / 100
+    transaction_fee = 0
 
-    def __init__(self, balance: float):
-        super().__init__(balance)
+    def __init__(self):
+        super().__init__()
 
-    def get_position(self) -> Position:
-        return self.position
+    def transaction(self,
+                    time: int,
+                    symbol: str,
+                    source_symbol: str,
+                    destination_symbol: str,
+                    source_symbol_amount: float,
+                    transaction_type: str,
+                    destination_symbol_amount: float = 0) -> None:
 
-    def buy(self,
-            time: int,
-            symbol: str,
-            price: float,
-            amount: float = 0) -> None:
+        if transaction_type == 'buy':
+            if destination_symbol_amount == 0:
+                destination_symbol_amount = self.balance[source_symbol] / source_symbol_amount
 
-        if amount == 0:
-            amount = self.balance / price
+            cost = destination_symbol_amount * source_symbol_amount  # Amount of initial symbol used
+            print(self.balance, ' selling ', source_symbol, ' amount ', source_symbol_amount)
+            self.balance[source_symbol] -= cost * (1 + self.transaction_percentage) + self.transaction_percentage
 
-        self.balance -= amount * price
-        log.info(f"{time} - Bought - {amount} of {symbol} at {price}")
-        self.position = Position(
-            Time=time,
-            Symbol=symbol,
-            Amount=amount,
-            Price=price
-        )
-        log.info(f"New position {self.position}")
-        self.list.append({
-            'Time': time,
-            'Action': 'Buy',
-            'Amount': amount,
-            'Symbol': symbol,
-            'Price': price
-        })
-
-    def sell(self,
-             time: int,
-             symbol: str,
-             price: float,
-             amount: float = 0) -> None:
-        if self.position.Time != time:
-            if amount == 0:
-                amount = self.position.Amount
-            self.balance += amount * price
-            log.info(f"{time} - Sold - {amount} of {symbol} at {price}")
-            log.info(f"New balance: {self.balance}")
-            self.position = Position()
-            self.list.append({
+            log.info(
+                f"{time} - Bought - {destination_symbol_amount} of {destination_symbol} at {source_symbol_amount} of {source_symbol}  - Fee - {self.transaction_fee + cost * self.transaction_percentage}")
+            self.positions[destination_symbol] = Position(
+                Time=time,
+                Symbol=destination_symbol,
+                Amount=destination_symbol_amount,
+                Price=source_symbol_amount
+            )
+            log.info(f"New position {self.positions[destination_symbol]}")
+            self.lists[symbol] = [] if symbol not in self.lists else self.lists[symbol]
+            self.lists[symbol].append({
                 'Time': time,
-                'Action': 'Sell',
-                'Amount': amount,
-                'Symbol': symbol,
-                'Price': price
+                'Action': 'Buy',
+                'Amount': destination_symbol_amount,
+                'Symbol': destination_symbol,
+                'Price': source_symbol_amount
             })
+        if transaction_type == 'sell':
+            if self.positions[destination_symbol].Time != time:
+                if destination_symbol_amount == 0:
+                    destination_symbol_amount = self.positions[destination_symbol].Amount
+                self.balance[source_symbol] += destination_symbol_amount * source_symbol_amount
+                log.info(f"{time} - Sold - {destination_symbol_amount} of {destination_symbol} at {source_symbol_amount} of {source_symbol}")
+                log.info(f"New balance: {self.balance[source_symbol]}")
+                self.positions[destination_symbol] = Position()
+                self.lists[symbol] = [] if symbol not in self.lists else self.lists[symbol]
 
-    def get_plot(self) -> go:
-        buys = [d for d in self.list if d["Action"] == "Buy"]
-        sells = [d for d in self.list if d["Action"] == "Sell"]
+                self.lists[symbol].append({
+                    'Time': time,
+                    'Action': 'Sell',
+                    'Amount': destination_symbol_amount,
+                    'Symbol': destination_symbol,
+                    'Price': source_symbol_amount
+                })
+
+    def get_plot(self, symbol: str) -> go:
+        buys = [d for d in self.lists[symbol] if d["Action"] == "Buy"]
+        sells = [d for d in self.lists[symbol] if d["Action"] == "Sell"]
         return go.Scatter(
             x=[d["Time"] for d in buys],
             y=[d["Price"] for d in buys],
@@ -76,6 +82,15 @@ class TestAccount(Account):
             mode="markers",
             name="Sell"
         )
+
+    def _get_balance(self) -> Dict[str, float]:
+        return {
+            'USDT': 100,
+            'ADA': 0,
+            'BUSD': 200,
+            'XRP': 0,
+            'BTC': 0
+        }
 
     @staticmethod
     def condition(name: str) -> bool:
