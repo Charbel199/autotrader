fd155ce5cd6c13bf852d463caa60f2c707d246ca
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-13 15:18:47 +0200

    Add Account specific methods

--- AutoTrader/trading/accounts/account.py
+++ AutoTrader/trading/accounts/account.py
@@ -4,20 +4,20 @@ import plotly.graph_objects as go
 from AutoTrader.exceptions import AccountNotFound
 from AutoTrader.services.summary import get_trades_summary, print_summary
 from typing import Dict, List
-from AutoTrader.models import Transaction, Position, Order
-from AutoTrader.enums import *
+from AutoTrader.models import Order
+from AutoTrader.enums import OrderSide, OrderType
 import copy
 
 log = logger.get_logger(__name__)
 
 
 class Account(ABC):
-    columns = ['Time', 'Action', 'Amount', 'Symbol', 'Price']
+    # columns = ['Time', 'Action', 'Amount', 'Symbol', 'Price']
     transaction_percentage = NotImplemented
     transaction_fee = NotImplemented
 
     def __init__(self):
-        balance = self._get_balance()
+        balance = self._get_account_initial_balance()
         self.balance: Dict[str, float] = balance
         self.initial_balance: Dict[str, float] = copy.deepcopy(balance)
         self.orders: Dict[str, List[Order]] = {}
@@ -27,18 +27,37 @@ class Account(ABC):
     def get_position(self, symbol: str) -> Order:
         return self.positions.get(symbol, Order())
 
-    @abstractmethod
-    def place_order(self,
-                    time: int,
-                    symbol: str,
-                    source_symbol: str,
-                    destination_symbol: str,
-                    type: OrderType,
-                    price: float,
-                    side: OrderSide,
-                    source_symbol_total_amount: float = 0,
-                    destination_symbol_amount: float = 0) -> None:
-        pass
+    def get_orders(self, symbol: str) -> List[Order]:
+        return self.orders.get(symbol, [])
+
+    def get_open_orders(self, symbol: str) -> List[Order]:
+        return self.open_orders.get(symbol, [])
+
+    def add_order(self, symbol: str, order: Order) -> None:
+        self.orders[symbol] = [] if symbol not in self.orders else self.orders[symbol]
+        self.orders[symbol].append(order)
+
+    def add_open_order(self, symbol: str, order: Order) -> None:
+        self.open_orders[symbol] = [] if symbol not in self.open_orders else self.open_orders[symbol]
+        self.open_orders[symbol].append(order)
+
+    def set_position(self, symbol: str, order: Order) -> None:
+        self.positions[symbol] = order
+
+    def add_to_balance(self, symbol: str, amount: float) -> None:
+        self.balance[symbol] = self.balance.get(symbol, 0) + amount
+
+    def remove_from_balance(self, symbol: str, amount: float) -> None:
+        self.balance[symbol] = self.balance.get(symbol) - amount
+
+    def reset_position(self, symbol: str) -> None:
+        self.positions[symbol] = Order()
+
+    def get_symbol_balance(self, symbol: str) -> None:
+        self.get_current_balance().get(symbol, 0)
+
+    def refresh_balance(self) -> None:
+        self.balance = self.get_current_balance()
 
     def get_profit(self, symbol: str, primary_symbol: str, secondary_symbol: str, undo_last_position=True) -> float:
         # If in position when calculating profit, revert last buy
@@ -58,26 +77,57 @@ class Account(ABC):
         summary = get_trades_summary(self.orders[symbol], self.initial_balance[primary_symbol])
         return summary['AllTrades']
 
-    @abstractmethod
     def get_plot(self, symbol: str) -> go:
-        pass
-
-    @abstractmethod
-    def _get_balance(self) -> Dict[str, float]:
-        pass
+        buys = [d for d in self.orders[symbol] if d.Side == OrderSide.BUY]
+        sells = [d for d in self.orders[symbol] if d.Side == OrderSide.SELL]
+        return go.Scatter(
+            x=[d.Time for d in buys],
+            y=[d.Price for d in buys],
+            marker=dict(color="gold", size=13, symbol=46),
+            mode="markers",
+            name="Buy"
+        ), go.Scatter(
+            x=[d.Time for d in sells],
+            y=[d.Price for d in sells],
+            marker=dict(color="silver", size=13, symbol=45),
+            mode="markers",
+            name="Sell"
+        )
 
     def _undo_last_position(self, symbol: str, primary_symbol: str, secondary_symbol: str) -> None:
         # If no position found with this symbol, return
         if symbol not in self.positions:
             return
 
-        if self.positions[symbol].is_valid():
+        position = self.get_position(symbol)
+        if position.is_valid():
             # Remove last transaction
             self.orders[symbol] = self.orders[symbol][:-1]
             # Refund balance
-            self.balance[primary_symbol] += self.positions[symbol].Price * self.positions[symbol].ExecutedQuantity
+            self.balance[primary_symbol] += position.Price * position.ExecutedQuantity
             # Empty position
-            self.positions[symbol] = Order()
+            self.reset_position(symbol)
+
+    @abstractmethod
+    def get_current_balance(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def _get_account_initial_balance(self) -> Dict[str, float]:
+        pass
+
+    @abstractmethod
+    def place_order(self,
+                    time: int,
+                    symbol: str,
+                    source_symbol: str,
+                    destination_symbol: str,
+                    order_type: OrderType,
+                    price: float,
+                    side: OrderSide,
+                    amount: float
+                    ) -> None:
+        pass
 
     @staticmethod
     @abstractmethod
--- AutoTrader/trading/accounts/account_test.py
+++ AutoTrader/trading/accounts/account_test.py
@@ -1,8 +1,7 @@
 from AutoTrader.trading.accounts.account import Account
-import plotly.graph_objects as go
 from AutoTrader.helper import logger
-from AutoTrader.models import Position, Transaction, Order
-from AutoTrader.enums import *
+from AutoTrader.models import  Order
+from AutoTrader.enums import OrderType, OrderSide
 from AutoTrader.helper.date_helper import from_timestamp_to_date
 from typing import Dict
 
@@ -12,6 +11,13 @@ log = logger.get_logger(__name__)
 class AccountTest(Account):
     transaction_percentage = 0.1 / 100
     transaction_fee = 0
+    account_balance = {
+        'USDT': 100,
+        'ADA': 0,
+        'BUSD': 200,
+        'XRP': 0,
+        'BTC': 0
+    }
 
     def __init__(self):
         super().__init__()
@@ -21,106 +27,76 @@ class AccountTest(Account):
                     symbol: str,
                     source_symbol: str,
                     destination_symbol: str,
-                    type: OrderType,
+                    order_type: OrderType,
                     price: float,
                     side: OrderSide,
-                    source_symbol_total_amount: float = 0,
-                    destination_symbol_amount: float = 0) -> None:
+                    amount: float) -> None:
 
         if side == OrderSide.BUY:
             # Subtract the total amount of source symbol spent
-            self.balance[source_symbol] -= source_symbol_total_amount
+            source_symbol_total_amount = amount * price
+            self.remove_from_balance(source_symbol, source_symbol_total_amount)
 
             # Apply transaction fees (Orders are executed immediately as this is a 'test account')
             fees = source_symbol_total_amount * self.transaction_percentage + self.transaction_fee
-            source_symbol_total_amount -= fees
-
-            # Calculate the amount of destination_symbol that will be bought
-            destination_symbol_amount = source_symbol_total_amount / price
+            # Adjusted amount after fees
+            amount = (source_symbol_total_amount - fees) / price
 
             # Add the total amount of destination symbol bought
-            self.balance[destination_symbol] += destination_symbol_amount
+            self.add_to_balance(destination_symbol, amount)
 
             log.info(
-                f"{from_timestamp_to_date(time)} - Bought - {destination_symbol_amount} of {destination_symbol} for {source_symbol_total_amount} of {source_symbol}, {price} per token  - Fee - {fees}")
+                f"{from_timestamp_to_date(time)} - Buy Order - {amount} of {destination_symbol} for {source_symbol_total_amount} of {source_symbol}, {price} per token  - Fee - {fees}")
             # Add transaction
-            self.orders[symbol] = [] if symbol not in self.orders else self.orders[symbol]
-            self.orders[symbol].append(Order(
-                Time=time,
-                Side=side,
-                OriginalQuantity=destination_symbol_amount,
-                ExecutedQuantity=destination_symbol_amount,
-                Symbol=symbol,
-                Type=type,
-                Price=price
-            ))
-
-            self.positions[symbol] = self.orders[symbol][-1]
-            print(self.positions[symbol])
+            self.add_order(symbol=symbol,
+                           order=Order(
+                               Time=time,
+                               Side=side,
+                               OriginalQuantity=amount,
+                               ExecutedQuantity=amount,
+                               Symbol=symbol,
+                               Type=order_type,
+                               Price=price
+                           ))
+            self.set_position(symbol=symbol, order=self.orders[symbol][-1])
             log.info(f"New position {self.positions[symbol]}")
 
         if side == OrderSide.SELL:
-            # Sell only if position time is different from current time
-            if self.positions[symbol].Time == time:
-                return
-
-            # Set amount of destination symbol
-            destination_symbol_amount = self.positions[symbol].ExecutedQuantity
-
             # Subtract the total amount of destination symbol spent
-            self.balance[destination_symbol] -= destination_symbol_amount
+            self.remove_from_balance(destination_symbol, amount)
 
             # Apply transaction fees (Orders are executed immediately as this is a 'test account')
-            fees = destination_symbol_amount * self.transaction_percentage + self.transaction_fee
-            destination_symbol_amount -= fees
+            fees = amount * self.transaction_percentage + self.transaction_fee
+            # Actual amount that will be sold
+            amount -= fees
 
             # Amount of source symbol bought
-            source_symbol_total_amount = destination_symbol_amount * price
-            self.balance[source_symbol] += source_symbol_total_amount
+            source_symbol_total_amount = amount * price
+
+            self.add_to_balance(source_symbol, source_symbol_total_amount)
+
             log.info(
-                f"{from_timestamp_to_date(time)} - Sold - {destination_symbol_amount} of {destination_symbol} for {source_symbol_total_amount} of {source_symbol}, {price} per token  - Fee - {fees}")
+                f"{from_timestamp_to_date(time)} - Sell Order - {amount} of {destination_symbol} for {source_symbol_total_amount} of {source_symbol}, {price} per token  - Fee - {fees}")
 
-            self.orders[symbol] = [] if symbol not in self.orders else self.orders[symbol]
-            self.orders[symbol].append(Order(
-                Time=time,
-                Side=side,
-                OriginalQuantity=destination_symbol_amount,
-                ExecutedQuantity=destination_symbol_amount,
-                Symbol=symbol,
-                Type=type,
-                Price=price
-            ))
+            self.add_order(symbol=symbol,
+                           order=Order(
+                               Time=time,
+                               Side=side,
+                               OriginalQuantity=amount,
+                               ExecutedQuantity=amount,
+                               Symbol=symbol,
+                               Type=order_type,
+                               Price=price
+                           ))
 
-            self.positions[symbol] = Order()
-            log.info(f"New balance: {self.balance[source_symbol]}")
+            self.reset_position(symbol)
+            log.info(f"New balance: {self.get_symbol_balance(source_symbol)}")
 
-        print(self.balance)
+    def get_current_balance(self) -> Dict[str, float]:
+        return self.balance
 
-    def get_plot(self, symbol: str) -> go:
-        buys = [d for d in self.orders[symbol] if d.Side == OrderSide.BUY]
-        sells = [d for d in self.orders[symbol] if d.Side == OrderSide.SELL]
-        return go.Scatter(
-            x=[d.Time for d in buys],
-            y=[d.Price for d in buys],
-            marker=dict(color="gold", size=13, symbol=46),
-            mode="markers",
-            name="Buy"
-        ), go.Scatter(
-            x=[d.Time for d in sells],
-            y=[d.Price for d in sells],
-            marker=dict(color="silver", size=13, symbol=45),
-            mode="markers",
-            name="Sell"
-        )
-
-    def _get_balance(self) -> Dict[str, float]:
-        return {
-            'USDT': 100,
-            'ADA': 0,
-            'BUSD': 200,
-            'XRP': 0,
-            'BTC': 0
-        }
+    def _get_account_initial_balance(self) -> Dict[str, float]:
+        return self.account_balance
 
     @staticmethod
     def condition(name: str) -> bool:
