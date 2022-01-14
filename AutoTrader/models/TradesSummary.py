7666df501526c9bb8642cb75eaf536b5789c7739
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-14 17:58:04 +0200

    Add trades summary model

---
+++ AutoTrader/models/TradesSummary.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from typing import List, Dict
+from AutoTrader.models import Trade
+
+
+@dataclass
+class TradesSummary(object):
+    """
+    Trades Summary
+
+    Note: A trade here refers to entering then exiting a position (Buy and Sell)
+    """
+
+    # Balance
+    InitialQuoteBalance: float = 0
+    EndQuoteBalance: float = 0
+    PercentageQuoteChange: float = 0
+
+    # Trades
+    NumberOfTrades: int = 0
+    NumberOfWinningTrades: int = 0
+    NumberOfLoosingTrades: int = 0
+    Trades: List[Trade] = List
+
+    # Profit
+    AverageProfitPerTrade: float = 0
+    AverageProfitPerWinningTrade: float = 0
+    AverageLossPerLoosingTrade: float = 0
+
+    # Hold time
+    AveragePositionHoldTime: float = 0
+    AveragePositionHoldTimeWin: float = 0
+    AveragePositionHoldTimeLoss: float = 0
+
+    # Streaks
+    LongestNumberOfTradesWinningStreak: int = 0
+    LongestNumberOfTradesLosingStreak: int = 0
+
+    # Largest Wins and Losses
+    LargestProfit: float = 0
+    LargestLoss: int = 0
+
+    # Fees
+    Fees: Dict[str, float] = Dict
+
+    # Duration
+    TotalTradesDuration: int = 0
+
+    def get_text_summary(self, show_trades=False) -> str:
+        summary_string = '\n'
+        for key, value in vars(self).items():
+            if key == 'Trades':
+                if not show_trades:
+                    continue
+                else:
+                    summary_string += f"{key: <35}->\n"
+                    for trade in value:
+                        summary_string += f"\t\t{trade}\n"
+
+            else:
+                summary_string += f"{key: <35}-> {value}\n"
+        return summary_string
--- AutoTrader/models/__init__.py
+++ AutoTrader/models/__init__.py
@@ -2,3 +2,5 @@ from .Tick import Tick
 from .Position import Position
 from .Order import Order
 from .Trade import Trade
+from .Symbol import Symbol
+from .TradesSummary import TradesSummary
