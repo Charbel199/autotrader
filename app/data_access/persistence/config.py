062985035588b010e0e8f3421266d6c864ba6b95
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-26 10:46:29 +0200

    Setup Database config and sessions

--- app/data_access/persistence/config.py
+++ app/data_access/persistence/config.py
@@ -8,12 +8,12 @@ class Settings:
     PROJECT_NAME: str = os.getenv("PROJECT_NAME")
     PROJECT_VERSION: str = os.getenv("PROJECT_VERSION")
 
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)  # default postgres port is 5432
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", 5432)  # default postgres port is 5432
+    DB_NAME: str = os.getenv("DB_NAME", "tdd")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 settings = Settings()
--- app/data_access/persistence/database.py
+++ app/data_access/persistence/database.py
@@ -1,13 +1,49 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from app.data_access.persistence.config import Settings
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from sqlalchemy import exc
+from sqlalchemy import event
+from sqlalchemy import select
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+load_dotenv()
 
+DATABASE_CONNECTION_URL = Settings.DATABASE_URL
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_CONNECTION_URL,
+    pool_size=20,
+    max_overflow=0,
+    pool_pre_ping=True,
+    pool_timeout=30
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+@event.listens_for(engine, "engine_connect")
+def ping_connection(connection, branch):
+    if branch:
+        # "branch" refers to a sub-connection of a connection,
+        # we don't want to bother pinging on these.
+        return
+
+    try:
+        # run a SELECT 1.   use a core select() so that
+        # the SELECT of a scalar value without a table is
+        # appropriately formatted for the backend
+        connection.scalar(select(1))
+    except exc.DBAPIError as err:
+        # catch SQLAlchemy's DBAPIError, which is a wrapper
+        # for the DBAPI's exception.  It includes a .connection_invalidated
+        # attribute which specifies if this connection is a "disconnect"
+        # condition, which is based on inspection of the original exception
+        # by the dialect in use.
+        if err.connection_invalidated:
+            # run the same SELECT again - the connection will re-validate
+            # itself and establish a new connection.  The disconnect detection
+            # here also causes the whole connection pool to be invalidated
+            # so that all stale connections are discarded.
+            connection.scalar(select(1))
+        else:
+            raise
+
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
