2549472f9d04e1cd3ae74f1e85d065c1593aae65
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-29 16:44:59 +0200

    Add token verification

--- app/api/controllers/authentication_controller.py
+++ app/api/controllers/authentication_controller.py
@@ -3,12 +3,11 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from app.core.entities import entity_user
 from app.application.application_dependency_injection import Managers
 from app.application.services.interfaces.abstract_authentication_service import AbstractAuthenticationService
+from app.api.dependencies.user_dependency import get_current_active_user
 
 router = APIRouter()
-oauth_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 authentication_manager: AbstractAuthenticationService = Managers.authentication_manager()
-
-JWT_SECRET = "test"
 
 
 @router.post("/token")
@@ -20,3 +19,9 @@ async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
 @router.post("/register", response_model=entity_user.UserCreateResponse)
 async def create_user(user_data: entity_user.UserCreate):
     return await authentication_manager.register_user(user_data)
+
+
+# Test method to be removed
+@router.get("/user/me", response_model=entity_user.UserToken)
+async def current_user_information(current_user: entity_user.UserToken = Depends(get_current_active_user)):
+    return current_user
