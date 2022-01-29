8365a5a3b3e5659c1591786a0cb4892a6821ed6f
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-29 03:47:19 +0200

    Add add user functionality in repository

--- app/data_access/repositories/interfaces/abstract_user_repository.py
+++ app/data_access/repositories/interfaces/abstract_user_repository.py
@@ -1,7 +1,10 @@
 from abc import ABC, ABCMeta, abstractmethod
 from app.data_access.persistence.models import user
-
+from app.core.entities import entity_user
 
 class AbstractUserRepository(ABC):
     @abstractmethod
     async def get_user_from_email(self, email: str) -> user.User: raise NotImplementedError
+
+    @abstractmethod
+    async def add_user(self, user_data: entity_user.UserCreate) -> user.User: raise NotImplementedError
--- app/data_access/repositories/user_repository.py
+++ app/data_access/repositories/user_repository.py
@@ -1,9 +1,23 @@
 from .interfaces.abstract_user_repository import AbstractUserRepository
 from app.data_access.persistence.models import user
 from app.data_access.persistence.database import Session
+from ...core.entities import entity_user
 
 
 class UserRepository(AbstractUserRepository):
+    async def add_user(self, user_data: entity_user.UserCreate) -> user.User:
+        session = Session()
+        new_user = user.User(
+            email=user_data.email,
+            hashed_password=user_data.password,
+            first_name=user_data.first_name,
+            last_name=user_data.last_name
+        )
+        session.add(new_user)
+        session.commit()
+        session.close()
+        return new_user
+
     async def get_user_from_email(self, email: str) -> user.User:
         session = Session()
         user_from_db = session.query(user.User).filter(user.User.email == email).first()
