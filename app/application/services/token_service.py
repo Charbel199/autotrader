d5563a52b2de33121710f8bd5f951f958bc7d7cd
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-29 03:56:19 +0200

    Remove unnecessary import

--- app/application/services/token_service.py
+++ app/application/services/token_service.py
@@ -1,8 +1,9 @@
 from typing import Dict
 import jwt
 from app.application.services.interfaces.abstract_token_service import AbstractTokenService
-from app.core.entities import entity_user
 import os
+
+
 class TokenService(AbstractTokenService):
     async def encode_token(self, payload: Dict) -> str:
         return jwt.encode(payload, os.getenv("JWT_SECRET"))
