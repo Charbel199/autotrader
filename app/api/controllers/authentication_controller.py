bcc2e3b16159dfe4e487429b7b609bf798c7dcf1
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-29 03:46:26 +0200

    Add proper authentication endpoints

--- app/api/controllers/authentication_controller.py
+++ app/api/controllers/authentication_controller.py
@@ -1,34 +1,22 @@
-from typing import Optional
-from fastapi import Depends, FastAPI, HTTPException, status,APIRouter, Response
+from fastapi import Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-
-
+from app.core.entities import entity_user
+from app.application.application_dependency_injection import Managers
+from app.application.services.interfaces.abstract_authentication_service import AbstractAuthenticationService
 
 router = APIRouter()
 oauth_scheme = OAuth2PasswordBearer(tokenUrl='token')
+authentication_manager: AbstractAuthenticationService = Managers.authentication_manager()
+
+JWT_SECRET = "test"
+
 
 @router.post("/token")
-async def token_generate(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
-        "disabled": False,
-    }
-    print(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    # user = UserInDB(**user_dict)
-    print(form_data.password)
-    # hashed_password = fake_hash_password(form_data.password)
-    # if not hashed_password == user.hashed_password:
-    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    token = await authentication_manager.authenticate_user(email=form_data.username, password=form_data.password)
+    return {"access_token": token, "token_type": "bearer"}
 
-    return {"access_token": user_dict['username'], "token_type": "bearer"}
 
-@router.get("/gift")
-async def get_gift(token: str = Depends(oauth_scheme)):
-    print(token)
-    return {"gift":"HEY"}
+@router.post("/register", response_model=entity_user.UserCreateResponse)
+async def create_user(user_data: entity_user.UserCreate):
+    return await authentication_manager.register_user(user_data)
