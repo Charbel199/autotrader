b502a7214db6d43e19d7295271e262647b9ac7eb
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-30 15:37:01 +0200

    Remove authentication manager

--- app/application/application_dependency_injection.py
+++ app/application/application_dependency_injection.py
@@ -1,19 +1,15 @@
 from dependency_injector import containers, providers
 from app.application.services.interfaces.abstract_user_service import AbstractUserService
-from app.application.services.interfaces.abstract_authentication_service import AbstractAuthenticationService
 from app.application.services.interfaces.abstract_token_service import AbstractTokenService
+from app.application.services.interfaces.abstract_bot_service import AbstractBotService
 from app.application.services.user_service import UserService
-from app.application.services.authentication_service import AuthenticationService
 from app.application.services.token_service import TokenService
+from app.application.services.bot_service import BotService
 from app.data_access.data_access_dependency_injection import Repositories
 
 
 class Services(containers.DeclarativeContainer):
-    user_service = providers.Factory(AbstractUserService.register(UserService), user_repository=Repositories.user_repository)
     token_service = providers.Singleton(AbstractTokenService.register(TokenService))
-
-
-class Managers(containers.DeclarativeContainer):
-    authentication_manager = providers.Factory(AbstractAuthenticationService.register(AuthenticationService),
-                                               user_service=Services.user_service,
-                                               token_service=Services.token_service)
+    user_service = providers.Factory(AbstractUserService.register(UserService), user_repository=Repositories.user_repository, token_service=token_service)
+    bot_service = providers.Factory(AbstractBotService.register(BotService),
+                                    bot_repository=Repositories.bot_repository)
