fb5196b97a9c8770b95f779ff06a9a953eabbc6c
Charbel199 <charbel-boumaroun@outlook.com>
2022-01-30 15:38:51 +0200

    Update user dependency file based on entity change

--- app/api/dependencies/user_dependency.py
+++ app/api/dependencies/user_dependency.py
@@ -1,21 +1,21 @@
 from typing import Optional
-
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from app.application.services.interfaces.abstract_authentication_service import AbstractAuthenticationService
-from app.core.entities import entity_user
-from app.application.application_dependency_injection import Managers
+from app.application.models import user_model
+from app.application.services.interfaces.abstract_user_service import AbstractUserService
+from app.application.application_dependency_injection import Services
 from app.application.exceptions.not_found_exception import NotFoundException
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"token")
-authentication_manager: AbstractAuthenticationService = Managers.authentication_manager()
+user_service: AbstractUserService = Services.user_service()
 
 
-async def get_user_from_token(token: str = Depends(oauth2_scheme)) -> Optional[entity_user.UserToken]:
-    user = await authentication_manager.get_current_active_user(token)
+async def get_user_from_token(token: str = Depends(oauth2_scheme)) -> Optional[user_model.UserToken]:
+    user = await user_service.get_current_active_user(token)
     return user
 
 
-def get_current_active_user(current_user: entity_user.UserToken = Depends(get_user_from_token)) -> Optional[entity_user.UserToken]:
+def get_current_active_user(current_user: user_model.UserToken = Depends(get_user_from_token)) -> Optional[user_model.UserToken]:
     if not current_user:
         raise NotFoundException()
     if not current_user.is_active:
